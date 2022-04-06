fa6cdfe650324bd2c393dcd4c537ae236b793980
robin ucar <mehmet.ucar@imediapro.co.uk>
2022-04-06 21:21:12 +0100

    create tuple

---
+++ tuples_sets.py
@@ -0,0 +1,9 @@
+# Create tuple
+
+fruits = ('Apples', 'Oranges', 'Grapes')
+# or using contructor
+
+fruits2 = tuple(('Apples', 'Oranges', 'Grapes'))
+
+print(fruits, fruits2)
+print(fruits, type(fruits))
