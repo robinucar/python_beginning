84b088518d208627e2b7d47075ae4f7407184c27
robin ucar <mehmet.ucar@imediapro.co.uk>
2022-04-06 15:52:25 +0100

    Multiple assignment and print assignments on the console

---
+++ variables.py
@@ -0,0 +1,12 @@
+x = 1  # int
+y = 2.5 # float
+
+name = 'Robin' #str
+
+is_developer = True # boolean
+
+# Multiple assignment
+
+x, y, name, is_cool = (1, 2.5, 'John', True)
+
+print(x, y, name, is_cool)
