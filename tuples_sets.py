c6f0f8cba11412bc387d581c6c57ccda3686e4bc
robin ucar <mehmet.ucar@imediapro.co.uk>
2022-04-06 21:31:56 +0100

    get value by index

--- tuples_sets.py
+++ tuples_sets.py
@@ -3,11 +3,20 @@
 fruits = ('Apples', 'Oranges', 'Grapes')
 # or using contructor
 
-fruits2 = tuple(('Apples', 'Oranges', 'Grapes'))
+# fruits2 = tuple(('Apples', 'Oranges', 'Grapes'))
 
-print(fruits, fruits2)
-print(fruits, type(fruits))
+# # or
+
+# fruits3 = "Javascript", "Python", "Ruby"
+
+# print(fruits, fruits2)
+# print(fruits, type(fruits))
+# print(fruits3, type(fruits3))
 
 # Single valu needs trailing comma :)
-fruit = ('Apples') # type return string
-fruit1 = ('Apples',) # type return tuple
+# fruit = ('Apples') # type return string
+# fruit1 = ('Apples',) # type return tuple
+
+# Get value by index
+
+print(fruits[1])
