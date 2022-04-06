148badac766dc73f5f3f195895eebfa0c53b38ca
robin ucar <mehmet.ucar@imediapro.co.uk>
2022-04-06 18:31:22 +0100

    get a value by index

--- lists.py
+++ lists.py
@@ -1,7 +1,12 @@
-# Create a list
-numbers = [1, 2, 3, 4, 5]
+# # Create a list
+# numbers = [1, 2, 3, 4, 5]
 
-# Use a constructor
-numbers2= list((1, 2, 3, 4, 5))
+# # Use a constructor
+# numbers2= list((1, 2, 3, 4, 5))
 
-print(numbers, numbers2)
+# print(numbers, numbers2)
+
+fruits = ['Apples', 'Oranges', 'Grapes', 'Pears']
+
+# Get a value by index
+print(fruits[1]) # output: Oranges
