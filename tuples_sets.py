d1b1e9f3aa9a47647ff20b0f9afe569a45e8765b
robin ucar <mehmet.ucar@imediapro.co.uk>
2022-04-06 21:23:10 +0100

    tupple single value

--- tuples_sets.py
+++ tuples_sets.py
@@ -7,3 +7,7 @@ fruits2 = tuple(('Apples', 'Oranges', 'Grapes'))
 
 print(fruits, fruits2)
 print(fruits, type(fruits))
+
+# Single valu needs trailing comma :)
+fruit = ('Apples') # type return string
+fruit1 = ('Apples',) # type return tuple
