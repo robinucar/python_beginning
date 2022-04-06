e5981128624bfcd85e03ca4b0047ad1691dfdfd5
robin ucar <mehmet.ucar@imediapro.co.uk>
2022-04-06 16:09:11 +0100

    formatting strings, argument by position

--- strings.py
+++ strings.py
@@ -3,4 +3,10 @@ age = 34
 
 # Concatenate
 
-print('Hello, my name is ' + name + 'and I am ' + str(age) + 'years old...')
+# print('Hello, my name is ' + name + 'and I am ' + str(age) + 'years old...')
+
+# String Formatting
+
+# Arguments by position
+
+print('My name is {name} and I am {age} years old...'.format(name = name, age = age))
