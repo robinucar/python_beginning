ef7ac9d5726d1423ecf78d0d2ae8cb913026925e
robin ucar <mehmet.ucar@imediapro.co.uk>
2022-04-07 21:36:12 +0100

    if else conditionls

---
+++ conditionals.py
@@ -0,0 +1,16 @@
+x = 10
+y = 5
+z = 50
+
+# Simple if
+# if x > y:
+#     print(f'{x} is greater than {y}') # output : 10 is greater than 5
+
+# If / else
+
+if x > z: 
+    print(f'{x} is greater than {z}')
+else:
+    print(f'{z} is greater than {y}') # output: 50 is greater than 5
+
+    
