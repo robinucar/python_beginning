88686be1818e9c219d2ad46b613c62ac32038a63
robin ucar <mehmet.ucar@imediapro.co.uk>
2022-04-07 21:25:35 +0100

    lambda function

--- functions.py
+++ functions.py
@@ -12,9 +12,14 @@
 
 # say_hello_default_arg() # output Hello Sam
 
-def get_sum(num1, num2):
-    total = num1 + num2
-    return total
+# def get_sum(num1, num2):
+#     total = num1 + num2
+#     return total
 
-result = get_sum(5, 15)
-print(result) # output 20
+# result = get_sum(5, 15)
+# print(result) # output 20
+
+# Lambda Function
+
+get_sum = lambda num1, num2 : num1 + num2
+print(get_sum(7,8)) # output 15 - Kind of Javascript arrow function -
