c62e00de95da830b70d08a416a241aa6b7659bdc
robin ucar <mehmet.ucar@imediapro.co.uk>
2022-04-07 20:55:46 +0100

    remove an item from dict

--- dictionaries.py
+++ dictionaries.py
@@ -35,9 +35,14 @@ person = {
 # print(person.items()) # output dict_items([('first_name', 'Robin Melsa'), ('last_name', 'Winters'), ('age', 34)])
 
 # Copy dict
-person2 = person.copy()
-print(person2) # output {'first_name': 'Robin Melsa', 'last_name': 'Winters', 'age': 34}
+# person2 = person.copy()
+# print(person2) # output {'first_name': 'Robin Melsa', 'last_name': 'Winters', 'age': 34}
 
-person2['city'] = 'London'
-print(person2)
-print(person)
+# person2['city'] = 'London'
+# print(person2)
+# print(person)
+
+# Remove Item
+
+del(person['age'])
+print(person) # output {'first_name': 'Robin Melsa', 'last_name': 'Winters'}
