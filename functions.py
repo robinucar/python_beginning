7dd1e9efdd942cc09f4cde14b8d0ea8a6ea46f69
robin ucar <mehmet.ucar@imediapro.co.uk>
2022-04-07 21:22:22 +0100

    function with return keyword

--- functions.py
+++ functions.py
@@ -1,13 +1,20 @@
 # Create function
 
-def say_hello(name):
-    print(f'Hello {name}')
+# def say_hello(name):
+#     print(f'Hello {name}')
 
-say_hello('Robin Winters') # output: Hello Robin Winters
+# say_hello('Robin Winters') # output: Hello Robin Winters
 
 # Function with default parametr
 
-def say_hello_default_arg(name = 'Sam'):
-    print(f'Hello {name}')
+# def say_hello_default_arg(name = 'Sam'):
+#     print(f'Hello {name}')
 
-say_hello_default_arg() # output Hello Sam
+# say_hello_default_arg() # output Hello Sam
+
+def get_sum(num1, num2):
+    total = num1 + num2
+    return total
+
+result = get_sum(5, 15)
+print(result) # output 20
