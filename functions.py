c68aa7f7dc83d63949fa2c2d55ede82eadf46047
robin ucar <mehmet.ucar@imediapro.co.uk>
2022-04-07 21:17:40 +0100

    function with default args

--- functions.py
+++ functions.py
@@ -4,3 +4,10 @@ def say_hello(name):
     print(f'Hello {name}')
 
 say_hello('Robin Winters') # output: Hello Robin Winters
+
+# Function with default parametr
+
+def say_hello_default_arg(name = 'Sam'):
+    print(f'Hello {name}')
+
+say_hello_default_arg() # output Hello Sam
