87ce152af5a1f20fa2a6f1b180fbb12ed7cca758
robin ucar <mehmet.ucar@imediapro.co.uk>
2022-04-07 20:40:21 +0100

    add key and value

--- dictionaries.py
+++ dictionaries.py
@@ -17,8 +17,13 @@ person = {
 
 # Get Value
 
-print(person['first_name']) # output: Robin Melsa
+# print(person['first_name']) # output: Robin Melsa
 
-# Get value II Way
+# # Get value II Way
 
-print(person.get('last_name'))
+# print(person.get('last_name')) # output Winters
+
+# Add key/value
+person['phone'] = '555-555-5555'
+print(person.get('phone')) # output 555-555-5555
+print(person) # output {'first_name': 'Robin Melsa', 'last_name': 'Winters', 'age': 34, 'phone': '555-555-5555'}
