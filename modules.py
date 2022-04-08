ea8b99ba8bf91b6a23980971a51cf64fa363de59
robin ucar <mehmet.ucar@imediapro.co.uk>
2022-04-08 16:04:04 +0100

    import core module which are available with python by defaul

---
+++ modules.py
@@ -0,0 +1,15 @@
+# A module is basically a file containing a set of functions to include in an application.
+# There are core python modules, modules can be install using the pip package manager
+# (including Django) as well as custom modules
+
+# import a core module
+
+import datetime
+
+today = datetime.date.today()
+print(today) # output: 2022-04-08
+
+# or
+from datetime import date
+today = date.today()
+print(today) # output: 2022-04-08
