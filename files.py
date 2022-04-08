85c0d5400b095fe6d50067db9a03bbd8fd8ad222
robin ucar <mehmet.ucar@imediapro.co.uk>
2022-04-08 17:44:00 +0100

    Append to file, read from the file

--- files.py
+++ files.py
@@ -16,3 +16,13 @@ myFile.write('I am learning Python.')
 # Close file
 myFile.close()
 print('Is Closed: ', myFile.closed) # output: Is Closed:  True
+
+# Append to file
+myFile = open('myFile.txt', 'a')
+myFile.write(' I also would like to learn Django.')
+myFile.close()
+
+# Read from file
+myFile = open('myFile.txt', 'r+')
+text = myFile.read(100) # -100 is character number-
+print(text)
