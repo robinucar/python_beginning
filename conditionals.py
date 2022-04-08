9f4ebcf5eeb2f5225f434446ce9cab03f2ba2990
robin ucar <mehmet.ucar@imediapro.co.uk>
2022-04-08 12:25:52 +0100

    Membership operator in, not in

--- conditionals.py
+++ conditionals.py
@@ -36,14 +36,32 @@ z = 50
 # Logical Operators (and, or, not) - Used to combine conditional statements
 
 # and
-if x > 2 and x <= 10:
-    print('x is greater than 2 and less than or equal to 10')
+# if x > 2 and x <= 10:
+#     print('x is greater than 2 and less than or equal to 10') # output : x is greater than 2 and less than or equal to 10
 
 # or
-if x > 2 or x <= 10:
-    print('x is greater than 2 or less than or equal to 10')
+# if x > 2 or x <= 10:
+#     print('x is greater than 2 or less than or equal to 10') # output : x is greater than 2 or less than or equal to 10
 
 # not
-if not(x == y):
-    print(f'{x} is not equal {y}')
+# if not(x == y): # output : 10 is not equal 5
+#     print(f'{x} is not equal {y}')
+
+
+# Membership Operators (not, not in) - Membership operators are used to test if a 
+# sequence is presented in an object
+
+numbers = [1,2,3,4,5,6,7,8,9,10]
+
+# in
+if x in numbers:
+    print(x in numbers)
+
+# in
+if y in numbers:
+    print(y in numbers)
+
+# not in
+if z not in numbers:
+    print(z not in numbers)
 
